8075679efad015b0dc30fe2f9dff755cb1b9556e
soyombo <soyombo0@naver.com>
2023-01-30 17:48:10 +0300

    Initial Commit

---
+++ Splitter/SplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitterApp.swift
+//  Splitter
+//
+//  Created by Soyombo Mantaagiin on 30.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
